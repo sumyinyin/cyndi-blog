358cd0e59b73dfa9bae4dce6b70d7ad8639ddd1d
sumyinyin <nsi.cynthia@gmail.com>
2021-01-16 23:11:57 +0000

    Amend main.py

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
-import os
 
 # App initial set up
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
